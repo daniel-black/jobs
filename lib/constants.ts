ba88b02f563e39d198832bfa73c0fd8354f8b8f3
daniel-black <drblack651@gmail.com>
2022-12-15 17:48:05 -0700

    build out /api/jobs route

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,5 @@
-export const usaJobsApi = 'https://data.usajobs.gov/api/search';
+export const USER_AGENT = 'drblack651@gmail.com';
+export const API_KEY = process.env.USAJOBS_API_KEY;
+export const HOST = 'data.usajobs.gov';
+
+export const usaJobsApi = `https://${HOST}/api/search`;
--- pages/api/jobs.ts
+++ pages/api/jobs.ts
@@ -1,17 +1,57 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { HOST, usaJobsApi, USER_AGENT } from '../../lib/constants';
 
 type JobsApiResponse = {
-  name: string;
+  result: any;
 };
 
-export default function handler(
+const parameters = ['Keyword', 'PositionTitle', 'LocationName'];
+
+function isValidParameter(p: string): boolean {
+  return parameters.includes(p);
+} 
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<JobsApiResponse>
 ) {
+  const API_KEY = process.env.USAJOBS_API_KEY;
+  if (!API_KEY) {
+    throw new Error('No API key present');
+  }
 
-  const apiKey = process.env.USAJOBS_API_KEY;
-  console.log(`api key: "${apiKey}"`);
+  // Grab the query object off of the request
+  const { query } = req;
 
-  // this is dumb but I'm doing it to see if vercel has my env vars like they should
-  res.status(200).json({ name: `apiKey: "${apiKey}"` });
+  // Validate the query object
+  const queryKeys = Object.keys(query);
+  if (queryKeys.length === 0)
+    throw new Error('No query parameters were provided');
+
+  const urlSearchParams = new URLSearchParams();
+  for (const k of queryKeys) {
+    if (!isValidParameter(k))
+      throw new Error(`"${k}" is not a valid search parameter`);
+
+    if (typeof query[k] !== 'string')
+      throw new Error('Parameter values must be strings');
+
+    urlSearchParams.append(k, query[k]);
+  }
+
+  const url = usaJobsApi + '?' + urlSearchParams.toString();
+  
+  
+  const request = await fetch(url, {
+    method: 'GET',
+    headers: {
+      'Host': HOST,
+      'Authorization-Key': API_KEY,
+      'User-Agent': USER_AGENT,
+      'Content-Type': 'application/json',
+    }
+  });
+  const data = await request.json();
+
+  res.status(200).json({ result: data });
 }
