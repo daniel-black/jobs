d72e7258143d9c87aa3d59b2fd9622fc79a0968b
daniel-black <drblack651@gmail.com>
2022-12-16 22:15:27 -0700

    Try dynamic data fetching for /search

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,80 +1,80 @@
-// import { JobsApiResponse, Results } from "../../lib/types/jobsApiTypes";
-// import { getBaseUrl } from "../../lib/utils/general-utils";
-// import JobListing from "./JobListing";
+import { JobsApiResponse, Results } from "../../lib/types/jobsApiTypes";
+import { getBaseUrl } from "../../lib/utils/general-utils";
+import JobListing from "./JobListing";
 
-// type SearchParamsType = {
-//   PositionTitle?: string;
-//   LocationName?: string;
-//   Keyword?: string;
-// };
+type SearchParamsType = {
+  PositionTitle?: string;
+  LocationName?: string;
+  Keyword?: string;
+};
 
 export default async function SearchPage({ searchParams } : {
-  searchParams?: { [key: string]: string | string[] | undefined };
+  searchParams?: SearchParamsType
 }) {
 
-  return (
-    <>
-      <pre>😃{JSON.stringify(searchParams)}😃</pre>
-    </>
-  )
+  // return (
+  //   <>
+  //     <pre>😃{JSON.stringify(searchParams)}😃</pre>
+  //   </>
+  // )
 
-  // if (searchParams && Object.keys(searchParams).length === 0) {
-  //   return <div>Try searching for jobs!</div>;
-  // }
+  if (searchParams && Object.keys(searchParams).length === 0) {
+    return <div>Try searching for jobs!</div>;
+  }
 
-  // const urlSearchParams = new URLSearchParams(searchParams);
-  // const url = `${getBaseUrl()}/api/jobs?${urlSearchParams.toString()}`;
+  const urlSearchParams = new URLSearchParams(searchParams);
+  const url = `${getBaseUrl()}/api/jobs?${urlSearchParams.toString()}`;
   // console.log(url);
 
-  // const response = await fetch(url);
+  const response = await fetch(url, { cache: 'no-store' });
 
-  // if (!response.ok) {
-  //   return (
-  //     <div>
-  //       <p>API Error</p>
-  //       <p>{JSON.stringify(response, null, 2)}</p>
-  //     </div>
-  //   );
-  // }
+  if (!response.ok) {
+    return (
+      <div>
+        <p>API Error</p>
+        <p>{JSON.stringify(response, null, 2)}</p>
+      </div>
+    );
+  }
 
-  // const { result } = await response.json() as JobsApiResponse;
-  // console.log(result);
+  const { result } = await response.json() as JobsApiResponse;
+  console.log(result);
 
-  // if (result.hasOwnProperty('code')) {
-  //   return (
-  //     <div>
-  //       <p>API Error</p>
-  //       <p>{JSON.stringify(result, null, 2)}</p>
-  //     </div>
-  //   );
-  // }
+  if (result.hasOwnProperty('code')) {
+    return (
+      <div>
+        <p>API Error</p>
+        <p>{JSON.stringify(result, null, 2)}</p>
+      </div>
+    );
+  }
 
-  // const { count, countAll, jobs } = result as Results;
+  const { count, countAll, jobs } = result as Results;
 
-  // if (count === 0) return <p>No results found for {urlSearchParams.toString()}</p>;
+  if (count === 0) return <p>No results found for {urlSearchParams.toString()}</p>;
 
-  // const getResultsLine = (): string => {
-  //   return count === countAll 
-  //     ? (count === 1 ? 'Viewing only result' : `Viewing all ${count} results`) 
-  //     : `Viewing ${count} of ${countAll} results`;
-  // } 
+  const getResultsLine = (): string => {
+    return count === countAll 
+      ? (count === 1 ? 'Viewing only result' : `Viewing all ${count} results`) 
+      : `Viewing ${count} of ${countAll} results`;
+  } 
 
-  // const getSearchTerms = (): string => {
-  //   let terms = 'for ';
-  //   if (searchParams?.PositionTitle && searchParams.PositionTitle !== '') terms += `"${searchParams.PositionTitle}"`;
-  //   if (searchParams?.LocationName && searchParams.LocationName !== '') terms += `, "${searchParams.LocationName}"`;
-  //   if (searchParams?.Keyword && searchParams.Keyword !== '') terms += `, "${searchParams.Keyword}"`;
-  //   return terms;
-  // }
+  const getSearchTerms = (): string => {
+    let terms = 'for ';
+    if (searchParams?.PositionTitle && searchParams.PositionTitle !== '') terms += `"${searchParams.PositionTitle}"`;
+    if (searchParams?.LocationName && searchParams.LocationName !== '') terms += `, "${searchParams.LocationName}"`;
+    if (searchParams?.Keyword && searchParams.Keyword !== '') terms += `, "${searchParams.Keyword}"`;
+    return terms;
+  }
 
-  // return (
-  //   <div>
-  //     <p>{getResultsLine()} {getSearchTerms()}</p>
-  //     {jobs && jobs.length > 0 && (
-  //       <div className="space-y-3 px-3">
-  //         {jobs.map(job => <JobListing job={job} key={job.id} />)}
-  //       </div>
-  //     )} 
-  //   </div>
-  // );
+  return (
+    <div>
+      <p>{getResultsLine()} {getSearchTerms()}</p>
+      {jobs && jobs.length > 0 && (
+        <div className="space-y-3 px-3">
+          {jobs.map(job => <JobListing job={job} key={job.id} />)}
+        </div>
+      )} 
+    </div>
+  );
 }
